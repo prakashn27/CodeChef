40ebdfdffa7c7b44d68cc87590ef8516d808303b
prakashn27 <nprakashece@gmail.com>
2014-12-22 01:01:31 -0500

    added problem2 files

--- ProjectEuler-In-Swift/ProjectEuler-In-Swift/main.swift
+++ ProjectEuler-In-Swift/ProjectEuler-In-Swift/main.swift
@@ -11,3 +11,6 @@ import Foundation
 //problem 1
 var p1 = Problem1();  println(p1.solution())
 
+//problem2
+var p2 = Problem2(); println(p2.evenSum(4000000))
+
--- ProjectEuler-In-Swift/ProjectEuler-In-Swift/p1.swift
+++ ProjectEuler-In-Swift/ProjectEuler-In-Swift/p1.swift
@@ -6,12 +6,17 @@
 //  Copyright (c) 2014 prakashn. All rights reserved.
 //
 
+/* https://projecteuler.net/problem=1
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
 import Foundation
 class Problem1 {
     func sumOfNaturalNumbers(n: Float) -> Float {
         return (n) * (n + 1)/2
     }
-    //sumOfNaturalNumbers(4)
     
     func sumofMultiplesUptoM(m:Int, divisor:Int) -> Float {
         var numberOfElements = (m-1)/divisor    //sub 1 so as to not include the last element
