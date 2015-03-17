48ecd242160a6293dd8a2e6cf779866a23af1ed1
Prakash Natarajan <nprakashece@gmail.com>
2015-03-16 20:24:02 -0400

    Create Main-3-16-2015.java

---
+++ SPOJ/ADDREV/Main-3-16-2015.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+/*
+Sample input: 
+
+3
+24 1
+4358 754
+305 794
+
+Sample output:
+
+34
+1998
+1
+*/
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in = new Scanner(System.in);
+		int t=in.nextInt();
+		in.readLine();	//skip the remainig part of the line
+		while(in.hasNext()) {
+			String nums = in.readLine();
+			String[] num = nums.split(" "); //seperated by space
+			int a = reverse(num[0]);
+			int b = reverse(num[1]);
+			System.out.println(reverse(Integer.toString(a+b)));
+		}
+		
+	}
+	
+	public static int reverse(String n) {
+		StringBuilder sb = new StringBuilder(n);
+		return Integer.parseInt(sb.reverse().toString());
+	}
+}
