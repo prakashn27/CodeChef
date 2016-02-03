7a613dcdeff99cc3f61d2369bf7b216a0b24ff0e
prakashn27 <nprakashece@gmail.com>
2016-02-03 20:09:48 +0530

    Wrong answer :confused:

---
+++ SPOJ/src/NSTEPS.java
@@ -0,0 +1,44 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.math.*;
+
+/**
+ * @author prakashn
+ *
+ */
+public class NSTEPS {
+
+	/**
+	 * @param args
+	 */
+	static void p(Object o) { System.out.println(o); }
+	public static void main(String[] args) {
+		try {
+			Scanner in = new Scanner(System.in);
+			int t = in.nextInt();
+			in.nextLine();	
+			while(t-- > 0) {
+				int a = in.nextInt();
+				int b = in.nextInt();
+				in.nextLine();
+				if(Math.abs(a - b) == 2 || Math.abs(a - b) == 0) {
+//					boolean a_even = a % 2 == 0;
+//					boolean b_even = b % 2 == 0;
+					if(a % 2 == 0) {
+						System.out.println(a+b);
+					} else {
+						System.out.println(a+b-1);
+					}	
+				} else {
+					System.out.println("No Number");
+				}
+			}
+			
+		} catch(Exception e) {
+			
+		}
+		
+
+	}
+
+}
