e4513d3123e5811a80de586241f8059dd97e5618
Prakash Natarajan <nprakashece@gmail.com>
2016-04-29 09:11:06 -0400

    working on 11906

--- UVA-java/src/graph/KnightInWarGrid.java
+++ UVA-java/src/graph/KnightInWarGrid.java
@@ -3,7 +3,9 @@
  */
 package graph;
 
+import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * @author prakashn
@@ -24,27 +26,59 @@ public class KnightInWarGrid {
 	 */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int t, r, c, m, n, w;
-		int[][] table, paths;
+		int t, r, c, m, n, w, x, y;
+		int[][] water, paths;
 		boolean[][] visited;
 		
 		t = in.nextInt();
-		while(t-- != 0) {
+		for(int case_number = 1; case_number <= t; case_number++) {
 			r = in.nextInt();
 			c = in.nextInt();
 			m = in.nextInt();
 			n = in.nextInt();
 			w = in.nextInt();
-			table = new int[r][c];
+			water = new int[r][c];
 			paths = new int[r][c];
 			visited = new boolean[r][c];
-			
-			
-
-			
+			LinkedList<Position> possible_moves = new LinkedList<Position>();
+			possible_moves.add(new Position(m, n));
+			possible_moves.add(new Position(m, -n));
+			possible_moves.add(new Position(-m, n));
+			possible_moves.add(new Position(-m, -n));
+			possible_moves.add(new Position(n, m));
+			possible_moves.add(new Position(n, -m));
+			possible_moves.add(new Position(-n, m));
+			possible_moves.add(new Position(-n, -m));
+			for(int i = 0; i < r; i++) {
+				for(int j = 0; j < c; j++) {
+					water[i][j] = 0;
+					paths[i][j] = 0;
+					visited[i][j] = false;
+				}
+			}
+			for(int k = 0; k < w; k++) {
+				x = in.nextInt();
+				y = in.nextInt();
+				water[x][y] = 1;
+			}
+			Stack<Position> s = new Stack<Position>();
+			s.push(new Position(0, 0));
+			while(!s.isEmpty()) {
+				Position temp = s.pop();
+				// check out the posible moves for the temp;
+				// if it is valid and it is not water then push it into stack
+				// make sure to increment the count every time we go to path in the grid
+			}
 		}
 		in.close();
 
 	}
 
+}
+class Position {
+	int x, y;
+	Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 }
